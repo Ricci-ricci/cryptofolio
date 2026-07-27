811bc5f72ede52da4d72cc4879ed311c8fd0d088
Ricci-ricci <riccireese13@gmail.com>
2026-07-27 13:07:53 +0300

    update User endpoint

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse , NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { UpdateUserDTO, UserResponse } from "@/types/user_type";
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+export async function GET(request: NextRequest, { params }: PageProps) {
+  try {
+    const { id } = await params;
+    const users = await prisma.user.findUnique({ where: { id: id } })
+    if (users) return NextResponse.json({ id });
+  }
+  catch (error) {
+    return NextResponse.json(error , {status: 501})
+  }
+
+}
+export async function DELETE(request: NextRequest, { params }: PageProps) {
+  try {
+    const { id } = await params;
+    const users = await prisma.user.delete({ where: { id: id } })
+    if (users) return NextResponse.json({ message: "users deleted" });
+  }
+  catch (error) {
+    return NextResponse.json(error , {status: 501})
+  }
+
+}
+export async function UPDATE(request: NextRequest, { params }: PageProps) {
+  try {
+    const { id } = await params;
+    const users = await prisma.user.update({
+      where: { id: id }, data: {
+
+      }
+    })
+    if (users) return NextResponse.json({ message: "users update" });
+  }
+  catch (error) {
+    return NextResponse.json(error , {status: 501})
+  }
+
+}
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,30 @@
 import { NextResponse , NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
-export async function POST() {
-  const { } = NextRequest.json()
-  const users = await prisma.user.findMany();
+import { CreateUserDTO , UserResponse } from "@/types/user_type";
+export async function POST(request: NextRequest) {
+  try {
+    const { email , password }:CreateUserDTO = await request.json()
+    const users = await prisma.user.create({
+      data:
+      {
+        email: email,
+        password : password
+      }
+    });
+    return NextResponse.json(users, { status:200})
 
-  return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json(error, {status : 501})
+  }
+}
+export async function GET(request:NextRequest) {
+  try {
+    const { id, email }: UserResponse = await request.json()
+    const users = await prisma.user.findUnique({
+      where:{id : id}
+    })
+    if(users) return NextResponse.json(users , {status:201})
+  } catch(error) {
+    return NextResponse.json(error , {status:501})
+  }
 }
