ab2664dfde14859dc5c4343ab420ddaf239249e3
Ricci-ricci <riccireese13@gmail.com>
2026-07-27 18:22:35 +0300

    based Wallet manipulationo

--- app/api/prices/route.ts
+++ app/api/prices/route.ts
@@ -1,9 +1,21 @@
-import { NextRequest , NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
 export async function GET() {
   try {
-    const prices = await fetch("/link");
-    if (!prices) return NextResponse.json({ message: "failed to fetch Prices" });
-    return NextResponse.json(prices, { status: 200 });
+    const api_key = process.env.COIN_GECKO_API_KEY || ""
+    //curl --request GET \
+    //--url 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin&names=Bitcoin&symbols=btc' \
+    //--header 'x-cg-demo-api-key: <api-key>'
+    const prices = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd", {
+      method: "GET",
+      headers: {
+        'x-cg-demo-api-key':api_key
+      }
+    });
+    const data = await prices.json()
+    if (!data) return NextResponse.json({ message: "failed to fetch Prices" });
+    return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Internal server error" }, {status:500})
   }
--- app/api/transactions/[walletAddress]/route.ts
+++ app/api/transactions/[walletAddress]/route.ts
@@ -6,6 +6,8 @@ interface PageProps {
 export async function GET(request: NextRequest, { params } : PageProps) {
   try {
     const { walletAddress } = await params
+    const balances = await fetch("https://eth-mainnet.g.alchemy.com/v2/")
+
   } catch (error) {
     return NextResponse.json({ message: "Internal server Error" }, {status:500})
   }
