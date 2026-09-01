d83731b752b3e42dc738aac902aaeafe988f9a2b
Ricci-ricci <riccireese13@gmail.com>
2026-09-01 13:17:34 +0300

    based middleware update

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -1,4 +1,14 @@
-const Middleware = () => {
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+const Middleware = async() => {
+  const session = await auth.api.getSession(
+    {
+      headers: await headers()
+    }
+  )
+  if (!session) {
+    return
+  }
   return
 }
 export default Middleware
